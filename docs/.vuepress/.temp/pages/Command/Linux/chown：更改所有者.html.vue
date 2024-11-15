fe72a9416b56fe60cd7a3fe6b09a52123039f498
LiPeng-China <lipeng_cn@foxmail.com>
2024-11-15 21:28:47 +0800

    fix: 20241115-删除 .vuepress/ 下的 .cache 和 .temp 目录

--- docs/.vuepress/.temp/pages/Command/Linux/chown：更改所有者.html.vue
+++
@@ -1,5 +0,0 @@
-<template><div><div class="language-bash line-numbers-mode" data-ext="sh"><pre v-pre class="language-bash"><code><span class="token comment"># -R: recursive 递归的持续变更，连同目录下的所有文件都变更</span>
-<span class="token function">sudo</span> <span class="token function">chown</span> user:user folder <span class="token parameter variable">-R</span>
-</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div></div></template>
-
-
